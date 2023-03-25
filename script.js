995b94f6cd3af9bf5dee2c415481d78cb6d6b748
Dipak Manohar Nimje <120239693+dipaknimje@users.noreply.github.com>
2023-03-25 14:10:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+btnId = document.getElementById("btn");
+colorId = document.getElementById("color");
+
+const updateColor = () => {
+ 
+  let randomNumber = Math.floor(Math.random() * 16777215);
+
+  let randomCode = "#" + randomNumber.toString(16);
+
+  document.body.style.backgroundColor = randomCode;
+  
+  colorId.innerText = randomCode;
+
+  btnId.style.backgroundColor = randomCode;
+
+  
+
+//   console.log(randomNumber , randomCode);
+
+navigator.clipboard.writeText(randomCode);
+};
+
+btnId.addEventListener("click" , () => {
+   updateColor();
+});
